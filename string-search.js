23cd75a400eef486bab62783af0bf9ed19c8a60e
Jubayer Hosen <jubayerhosain9838@gmail.com>
2022-02-14 02:37:02 +0800

    first commit

---
+++ string-search.js
@@ -0,0 +1,9 @@
+const products = [
+    'Dell hardcore i29 200GB laptop',
+    'iphone 1TB camera flashlight phone',
+    'yellow laptop with black camera',
+    '1X59 Lenove commercial yoga laptop',
+    'LG supernova laptop',
+    'HTC low price phone',
+    'purple color phone with laptop'
+]
