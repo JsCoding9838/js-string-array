eec8ef463be64cd91ff9ecd963e73344bf2d2988
Jubayer Hosen <jubayerhosain9838@gmail.com>
2022-02-15 02:58:17 +0800

    array function updated

---
+++ array-slice.js
@@ -0,0 +1,16 @@
+const numbers = [3, 6, 4, 8, 9, 19, 15, 21, 45, 87];
+// slice
+// const numberSliced = numbers.slice(4, 8);
+// console.log(numberSliced);
+// console.log(numbers);
+
+// splice to remove an element from an arrry
+// const numberSpliced = numbers.splice(4, 3);
+// console.log(numberSpliced);
+// console.log(numbers);
+
+// 
+// const numberSpliced2 = numbers.splice(4, 3, 99, 111, 888, 777);
+const numberSpliced2 = numbers.splice(4, 0, 777);
+console.log(numberSpliced2);
+console.log(numbers);
